de413bb177b44bbb38ab7fcf44b9197ba5cde9e6
Usman <ghaniusman0287@gmail.com>
2024-09-16 15:37:33 +0500

    first approach with class based component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-
+//class based component
 export default class App extends Component {
 
   apikey=process.env.REACT_APP_NEWS_API 
